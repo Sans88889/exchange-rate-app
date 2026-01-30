c4032bd759e83a53aa5013b773931c30dfc91327
Sans88889 <sandra.gabrysiak@gmail.com>
2026-01-30 13:02:21 +0100

    feat: add filtering and unit tests

--- exchange-rate-app/frontend/src/app/app.ts
+++ exchange-rate-app/frontend/src/app/app.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; // Dodajemy FormsModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule], // Dodaj FormsModule tutaj
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class AppComponent implements OnInit {
   currencies: any[] = [];
+  filteredCurrencies: any[] = []; // Tablica na odfiltrowane dane
   status: string = '';
+  searchTerm: string = ''; // Tu trafi tekst z wyszukiwarki
 
   constructor(private http: HttpClient) {}
 
@@ -23,12 +26,21 @@ export class AppComponent implements OnInit {
     this.http.get<any[]>('http://localhost:8000/currencies').subscribe({
       next: (data) => {
         this.currencies = data;
+        this.filterResults(); // Odśwież filtrowanie po pobraniu
       },
-      error: (err) => {
-        this.status = 'Błąd połączenia z API.';
-        console.error(err);
-      }
+      error: () => { this.status = 'Błąd połączenia.'; }
     });
+  }
+
+  filterResults(): void {
+    if (!this.searchTerm) {
+      this.filteredCurrencies = this.currencies;
+    } else {
+      this.filteredCurrencies = this.currencies.filter(c =>
+        c.currency.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        c.code.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
 
   fetchNewData(): void {
@@ -38,9 +50,7 @@ export class AppComponent implements OnInit {
         this.status = 'Zaktualizowano dane!';
         this.getRates();
       },
-      error: () => {
-        this.status = 'Błąd pobierania danych.';
-      }
+      error: () => { this.status = 'Błąd pobierania.'; }
     });
   }
 }
